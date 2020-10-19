e8b8cf22725820322b6fbc1e5bfcd25c0cad1a41
nanter1986 <nanter1986@gmail.com>
2020-10-19 18:08:53 +0300

    first

---
+++ first.py
@@ -0,0 +1,55 @@
+import random
+
+def attributeAssigner():
+    attribute=""
+    number=random.randint(0,5)
+    if number==0:
+        attribute="DARK"
+    elif number==1:
+        attribute="LIGHT"
+    elif number==2:
+        attribute="EARTH"
+    elif number==3:
+        attribute="WIND"
+    elif number==4:
+        attribute="FIRE"
+    elif number==5:
+        attribute="WATER"
+    return attribute
+
+def card_maker():
+    card=[]
+    att=random.randint(0,10)
+    card.append(att)
+    attr=attributeAssigner()
+    card.append(attr)
+    exploder=random.randint(0,1)
+    card.append(exploder)
+    returner=random.randint(0,1)
+    card.append(returner)
+    reborner=random.randint(0,1)
+    card.append(reborner)
+    drawer=random.randint(0,1)
+    card.append(drawer)
+    print(str(card))
+
+
+
+def printGap():
+    print("-------------------")
+
+
+def display_situation():
+    print("Player Life:")
+    print("100")
+    print("Player Force:")
+    print("50")
+    printGap()
+    print("Com Life:")
+    print("100")
+    print("Com Force:")
+    print("50")
+
+display_situation()
+for i in range(0,5):
+    card_maker()
